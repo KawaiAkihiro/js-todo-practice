97adbecd124f71698697185e769f9ad994ee20d5
KawaiAkihiro <72666840+KawaiAkihiro@users.noreply.github.com>
2021-05-10 00:33:03 +0900

    タスク追加処理・ボタンアラート処理

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import "./styles.css";
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する。
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //li生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //button(完了)タグの生成
+  const complateButton = document.createElement("button");
+  complateButton.innerText = "完了";
+  complateButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button(削除)タグの生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(complateButton);
+  div.appendChild(deleteButton);
+
+  //未完了リストに追加
+  document.getElementById("imcomplate-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
